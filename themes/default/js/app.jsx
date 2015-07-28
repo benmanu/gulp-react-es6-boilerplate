87449219795256a46f9a28c2c777831f96677896
Ben Manu <ben@silverstripe.com>
2015-07-28 18:25:31 +1200

    Adding shims

--- themes/default/js/app.jsx
+++ themes/default/js/app.jsx
@@ -1,4 +1,14 @@
+/**
+ * @file App
+ * @module Default
+ * @requires module:babel/polyfill
+ * @requires module:react
+ * @requires module:./components/heading
+ */
+
 "use strict";
+
+require("babel/polyfill");
 
 var React = require('react');
 import Heading from './components/heading';
